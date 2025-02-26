e7bb9412467d11f4a6986046cd793767d16647be
sohany-shaik <sohanysaajid@gmail.com>
2025-02-26 20:51:30 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -5,15 +5,13 @@ col1,col2=st.columns(2)
 with col1:
     with st.container(height=400):
         st.image("images/photo.png",width=600)
-
 with col2:
     st.title("Shaik Sohany")
     content="""
     Hi am sohany shaik learning python to become python developer
     by changing my domain from NON-IT to IT software development.
     """
-    st.info(content)
-    
+    st.info(content)    
 content2 = """
 Below you can find some of the apps,i have built in python.
 feel free to contact me!
